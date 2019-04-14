eacd1ef40829720a0c949a3f0606ad322d3621aa
Bruno Rangel <bmsrangel@hotmail.com>
2019-04-14 15:50:24 -0300

    Aplicação front-end

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import Main from './pages/Main';
+import Routes from './routes';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+// Como o App não possui state, ele pode ser escrito na forma de função
+
+// function App() {
+// 	return <Main />;
+// }
+
+const App = () => <Routes />;
+
+// class App extends Component {
+// 	render() {
+// 		return <Main />;
+// 	}
+// }
 
 export default App;
